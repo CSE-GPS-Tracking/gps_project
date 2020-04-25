c189ae4a211eedaa95b8676fbb16c0cf508696c3
CSE-GPS-Tracking <CSEGPSTracking@gmail.com>
2020-04-25 08:46:37 +0530

    Bug fixes

--- gps_project/gps_databases/models.py
+++ gps_project/gps_databases/models.py
@@ -16,14 +16,14 @@ class User(models.Model):
     -- Roles : Student,Staff,Admin
     __ Attributes: user_id,password,firstName,lastName,phone,email_id,route_id,stop_id
     """
-    user_id = models.CharField(max_length=200),
-    password= models.CharField(max_length=200),
+    user_id = models.CharField(max_length=20)
+    password= models.CharField(max_length=30)
     first_name= models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30),
-    phone = models.CharField(max_length=10),
-    email_id = models.CharField(max_length=40),
-    route_id = models.CharField(max_length=40),
-    stop_id = models.CharField(max_length=10),
+    last_name = models.CharField(max_length=30)
+    phone = models.CharField(max_length=10)
+    email_id = models.CharField(max_length=40)
+    route_id = models.CharField(max_length=40)
+    stop_id = models.CharField(max_length=10)
     user_role_choices = [("Student","Student"),("Admin","Admin"),("Staff","Staff")]
     user_role = models.CharField(choices=user_role_choices,max_length=30)
 
@@ -37,10 +37,10 @@ class Driver(models.Model):
     driver_id,first_name,last_name,joining_date,end_date (Last day of job)
 
     """
-    driver_id = models.CharField(max_length=10),
-    first_name = models.CharField(max_length=30),
-    last_name = models.CharField(max_length=30),
-    joining_date = models.DateField(),
+    driver_id = models.CharField(max_length=10)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    joining_date = models.DateField()
     end_date= models.DateField()
 
 
